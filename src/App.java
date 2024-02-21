11222a34b1763b1f6685dc4edb74a0df09f1e0d0
arthur andrade <arthurandrad.23@gmail.com>
2024-02-21 14:13:48 -0300

    Calculator finished

--- src/App.java
+++ src/App.java
@@ -1,21 +1,38 @@
 import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         Scanner read = new Scanner(System.in);  // Create a Scanner object
 
-        System.out.println("Enter your first grade");
-        float firstGrade = read.nextFloat();  // Read user input
+        System.out.println("Enter your first number");
+        float firstNumber = read.nextFloat();
 
-        System.out.println("Enter your second grade");
-        float secondGrade = read.nextFloat();
-
-        float result = firstGrade + secondGrade;
-        float division = result / 2;
-        System.out.printf("Result is: %f", division);  // Output user input
-
-        //testing git
+        System.out.println("Enter your first number");
+        float secondNumber = read.nextFloat();
         
+        System.out.println("Select which operator you want to use");
+        System.out.println("1. Addition (+)");
+        System.out.println("2. Minus (-)");
+        System.out.println("3. Multiplication (*)");
+        System.out.println("4. Division (/)");
+        char operator = read.next().charAt(0);
+
+        if (operator == '+') {
+            float result = firstNumber + secondNumber;
+            System.out.println(result);
+        } else if (operator == '-') {
+            float result = firstNumber - secondNumber;
+            System.out.println(result);
+        } else if (operator == '*') {
+            float result = firstNumber * secondNumber;
+            System.out.println(result);
+        } else if (operator == '/') {
+            float result = firstNumber / secondNumber;
+            System.out.println(result);
+        } else {
+            System.out.println("Invalid operator");
+        }
+
         read.close();
     }
 }
